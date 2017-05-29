deb8e6f40b1ea7dcd24e5590a9be49ac12b9cdcf
Akhil Kumar <akhilnareshkumar@gmail.com>
2017-05-29 21:43:56 +0530

    Code for sliders, add comments.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function($scope, $http, $interval) {
-	$scope.look_x = 0;
-	$scope.look_y = 0;
-	$scope.look_z = 0;
+	// $scope.look_x = 0;
+	// $scope.look_y = 0;
+	// $scope.look_z = 0;
+	$scope.roll = 0;
+	$scope.pitch = 0;
+	$scope.yaw = 0;
 	$scope.pos_x = 0;
 	$scope.pos_y = 0;
-	$scope.pos_z = 500;
-	$scope.name = 'Akhil';
+	$scope.pos_z = 1;
 	var x_list;
 	$scope.fetched_data = []
-	console.log($scope.fetched_data);
 	var fetch = function (end) {
 		$http.get('json/overlay.json').then(function (response) {
 			$scope.fetched_data = response.data;
@@ -29,8 +30,6 @@ app.controller('myCtrl', function($scope, $http, $interval) {
 			renderer.setClearColor( 0xffffff, 0);
 			renderer.setSize( 320, 240 );
 
-
-
 			document.getElementById('canvas').appendChild( renderer.domElement );
 
 			var geometry = new THREE.Geometry();
@@ -44,7 +43,6 @@ app.controller('myCtrl', function($scope, $http, $interval) {
 				linewidth: 10,
 				linecap: 'round', //ignored by WebGLRenderer
 				linejoin:  'round' //ignored by WebGLRenderer
-
 			});
 
 			var line = new THREE.Line(geometry, material);
@@ -55,7 +53,6 @@ app.controller('myCtrl', function($scope, $http, $interval) {
 
 				$interval(function() {
 
-
 					$http.get('json/current_location_x.json').then(function(response) {
 						$scope.current_location = response.data;
 						console.log($scope.current_location);
@@ -63,44 +60,45 @@ app.controller('myCtrl', function($scope, $http, $interval) {
 
 						var fetch_orientation = function () {
 							
-								$http.get('json/current_orientation.json').then(function(response) {
-									$scope.current_orientation = response.data;
-									console.log($scope.current_orientation);
-									camera.position.set($scope.pos_x , $scope.pos_y , $scope.pos_z);
-									// camera.lookAt(new THREE.Vector3($scope.look_x, $scope.look_y, $scope.look_z));
-									camera.rotation.x = $scope.current_orientation[0];
-									camera.rotation.y = -$scope.current_orientation[2];
-									camera.rotation.z = $scope.current_orientation[1];
-									renderer.render(scene, camera);
-								});
-						
-						};
+							$http.get('json/current_orientation.json').then(function(response) {
+								$scope.current_orientation = response.data;
+								console.log($scope.current_orientation);
+								camera.position.set($scope.pos_x , $scope.pos_y , $scope.pos_z);
+								// camera.lookAt(new THREE.Vector3($scope.look_x, $scope.look_y, $scope.look_z));
+								// Uncomment next 3 lines and comment the 3 lines after that for using the sliders
+								camera.rotation.x = $scope.roll;
+								camera.rotation.y = -$scope.pitch;
+								camera.rotation.z = $scope.yaw;
+								// camera.rotation.x = $scope.current_orientation[0];
+								// camera.rotation.y = -$scope.current_orientation[2];
+								// camera.rotation.z = $scope.current_orientation[1];
+								renderer.render(scene, camera);
+							});
 
-
+						}; // fetch_orintation() ends here
 
 						fetch_orientation();
 
 						
 
-					});
+					}); // $http.get().then() ends here
 
 
-				},1);
+				},1); // $interval ends here
 
 
-			};
-
+			}; // fetch_current_position ends here
 
 
 			fetch_current_position();
 
-		});
-	};
+
+		}); // $http.get().then() ends here
+
+
+	}; // fetch() ends here
 
 
 	fetch();
 
-
-
-});
-
+}); // controller ends here
